eaedab2d96713a4e7aaed31057c5223af935e241
momisalien <unicon0118@gmail.com>
2024-08-10 01:34:32 +0900

    3주차 django 개발 진행 중

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
+    path('', views.index),
+    path('<int:content_id>', views.detail,name='detail'),
 ]
 
