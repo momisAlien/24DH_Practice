dcd3089b7ac9c5f1db5333d01066361b496a5b7a
kim junseo <unicon0118@gmail.com>
2024-08-17 22:39:09 +0900

    4주차 django 개발 완료

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import MainContent
-# Register your models here.
+from .models import MainContent, Comment
+
+class MainContentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'pub_date']
+    search_fields = ['title']
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content_list', 'content', 'author', 'create_date', 'modify_date']
+    search_fields = ['author']
 
 admin.site.register(MainContent)
+admin.site.register(Comment)
